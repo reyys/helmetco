a924ef1d6f08ab8f6e808a24e7a3736e0429a28e
reyys <reyyy0207@gmail.com>
2022-05-27 22:13:48 +0700

    width changed

--- src/components/question/question1.js
+++ src/components/question/question1.js
@@ -9,8 +9,8 @@ function Question1() {
         <div className='italic question px-12 md:px-32 lg:px-64 flex flex-col w-full text-2xl justify-center h-screen'>
             <div className='font-bold text-center w-full'>{state.question}</div>
             <div className='flex mt-12 justify-center items-center flex-col md:flex-row gap-[20px]'>
-                <Link className="p-10 border-black border-[1px]" reloadDocument to={state.targeta}>{state.a}</Link>
-                <Link className="p-10 border-black border-[1px]" reloadDocument to={state.targetb}>{state.b}</Link>
+                <Link className="p-10 text-sm md:text-xl w-[200px] h-[100px] md:w-[300px] md:h-[150px] flex items-center justify-center text-center border-black border-[1px]" reloadDocument to={state.targeta}>{state.a}</Link>
+                <Link className="p-10 text-sm md:text-xl w-[200px] h-[100px] md:w-[300px] md:h-[150px] flex items-center justify-center text-center border-black border-[1px]" reloadDocument to={state.targetb}>{state.b}</Link>
             </div>
         </div>
     )
