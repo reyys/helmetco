7df0cc63b2365919e867d4db1feb3b6a1000d193
reyys <reyyy0207@gmail.com>
2022-05-27 18:27:46 +0700

    copy to clipboard feature

--- src/components/helmet/helmet.js
+++ src/components/helmet/helmet.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import dataHelmet from '../../helmet.js'
 import { useParams } from 'react-router-dom'
+import copyIcon from '../../assets/copy.png'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
 
 function Helmet() {
     const { id } = useParams()
+    const [coped, setCopied] = React.useState(false)
     const [helmet, setHelmet] = React.useState(dataHelmet[id])
+
+    const addOpacity = () => {
+        const clipBoard = document.querySelector('.clipboard')
+        clipBoard.classList.add('opacity')
+        setTimeout(() => {
+            clipBoard.classList.remove('opacity')
+        }, 500)
+    }
 
     const renderHelmet = () => {
         return (
@@ -14,9 +25,13 @@ function Helmet() {
                     <div className="font-bold text-2xl">Your helmet are...</div>
                     <div className='font-bold text-4xl'>{helmet.image}</div>
                     <div className='font-medium mt-12'>Claim your <span className='font-bold'>20% discount</span><br></br>with this promo code</div>
-                    <button id="copytoclip" className='button-copy flex relative justify-center items-center font-bold py-5 w-full mt-12 bg-[#ff9200] uppercase hover:scale-y-110 transition-all'>
-                        <span>{helmet.promo}</span>
-                    </button>
+                    <CopyToClipboard text={helmet.promo} onCopy={() => addOpacity()}>
+                        <button id="copytoclip" className='button-copy flex relative justify-center items-center font-bold py-5 w-full mt-12 bg-[#ff9200] uppercase hover:scale-y-110 transition-all'>
+                            <span>{helmet.promo}</span>
+                            <img className='ml-[15px] max-w-[30px]' src={copyIcon} alt="copy" />
+                            <span className='clipboard transition-all absolute right-[-50px] top-[-30px] p-3 rounded-lg bg-black text-white'>Copied</span>
+                        </button>
+                    </CopyToClipboard>
                 </div>
                 <img className="max-w-[300px]" src={helmet.imageurl} alt="" />
             </div>
